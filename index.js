3bf7484d9f0a3019c5092d6fe9d79c693406f3c6
Moses Erhinyodavwe <moseserhinyodavwe2@gmail.com>
2022-12-04 10:42:52 +0100

    sent to git

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // $('document').ready(function () {
+
 function searchWord() {
+
     var query = $('.searchInput').val();
-    if (query != ' ') {
+    if (query != '') {
         var settings = {
             "url": `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`,
             "method": "GET",
@@ -9,9 +11,39 @@ function searchWord() {
         };
 
         $.ajax(settings).done(function (response) {
-            console.log(response[0].phonetic);
+            console.log(response);
+            $('.mainTitle').text(response[0].word);
+            $('.phonetics').text(response[0].phonetic)
+
+            let meanings = response[0].meanings;
+            var files = '<div>'
+            for (let i = 0; i < meanings.length; i++) {
+                files += '<div ><p class="bg-dark text-light text-end p-2">' + meanings[i].partOfSpeech + '</p>';
+                var definitions = meanings[i].definitions;
+                var length = definitions.length;
+                for (let j = 0; j < length; j++) {
+                    files += '<div><p class="shadow-sm p-2 ">'
+                        + definitions[j].definition + "<br>" +
+                        "<i>" + "<span>" + 'example: ' + "</span>" + definitions[j].example + " </i>" + "</p>" + '</div>'
+                }
+                files += '</div>'
+            }
+            files += '</div>'
+            // console.log(files);
+            $('.def').html(files);
+
+            $('.license').html(
+                `${response[0].license.name} <br> ${response[0].license.url} `
+            )
         });
+    } else {
+        $('.mainTitle').text(' ');
+        $('.phonetics').text(' ')
+        $('.def').html(`<h4 class=" py-5 text-center display-4">Enter your text to search</h4>`)
     }
 }
 
-// })
+
+
+
+
